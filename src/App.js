731abdb47029284f6d8670dffef8a5490091c5a2
Sakshi-Jagtap1804 <sakshijagtap222@gmail.com>
2024-08-27 14:53:27 +0530

    new updates

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import Resturant from './component/Basics/Resturant';
+// import UseState from './component/Hooks/useState';
+// import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from './component/Hooks/useReducer';
+
+// export const App = () => {
+//   return <UseReducer />;
+// };
+
+// export default App;
+
+import React from 'react';
+// import Todo from './component/todoreact/todo';
+import Temp from './component/weather/temp';
+
+
+const App = () => {
+  return (
+    <>
+      <Temp />
+    </>
+  )
+}
+
+export default App;
